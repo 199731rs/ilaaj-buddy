9be1279d224f4883b7b0b1327d65f6b463e0ab67
ratnasingh <ratnasingh199731@gmail.com>
2026-05-08 14:12:04 +0530

    fix cors for verc el frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,9 +11,13 @@ app = FastAPI(
     version="1.0.0"
 )
 
+# ✅ FIXED CORS (important change)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+   # allow_origins=["*"], 
+    allow_origins=[
+        "https://ilaaj-buddy.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
